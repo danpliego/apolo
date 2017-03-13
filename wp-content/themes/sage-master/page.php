4ca11d28fcc5cbe379fdf37eb779d9823942cd07
Daniel Pliego <danielpliego@gmail.com>
2017-03-12 22:16:01 -0600

    add presskit

--- wp-content/themes/sage-master/page.php
+++ wp-content/themes/sage-master/page.php
@@ -1,155 +1,241 @@
 <?php while (have_posts()) : the_post(); ?>
   <?php get_template_part('templates/page', 'header'); ?>
-  <div class="top-header text-center">
-    <div class="container">
-      <div class="social-icons block">
-        <ul class="list-inline">
-          <li><a href="https://www.facebook.com/apolomx/?fref=ts"><img src="<?php echo get_bloginfo('template_directory');?>/assets/images/icons/facebook.png" alt=""></a></li>
-          <li><a href="https://www.instagram.com/apolomx/"><img src="<?php echo get_bloginfo('template_directory');?>/assets/images/icons/instagram.png" alt=""></a></li>
-          <li><a href="https://www.youtube.com/user/APOLOMXTV"><img src="<?php echo get_bloginfo('template_directory');?>/assets/images/icons/youtube.png" alt=""></a></li>
-          <li><a href="https://twitter.com/apolomx"><img src="<?php echo get_bloginfo('template_directory');?>/assets/images/icons/twitter.png" alt=""></a></li>
-        </ul>
-      </div>
-    </div>
-  </div>
-  <div class="header text-center">
-    <div class="container">
-      <div class="photo-container">
-        <img src="<?php echo get_bloginfo('template_directory');?>/assets/images/photo.png" alt="" class="photo">
-      </div>
-      <img src="<?php echo get_bloginfo('template_directory');?>/assets/images/noise-logo.png" alt="" class="photo-noise">
-      <img src="<?php echo get_bloginfo('template_directory');?>/assets/images/logo.png" alt="" class="logo">
-    </div>
-  </div>
-  <a href="#" class="scroll-icon text-center block">
-    <img src="<?php echo get_bloginfo('template_directory');?>/assets/images/scroll-icon.png" alt="">
-  </a>
-  <div class="container video block">
-    <div class="row">
-      <div class="col-sm-4 text-center">
-        <div class="block">
-          <p class="lead unmargin"><em>Nuevo Video</em></p>
-          <h1 class="unmargin-top">
-            Polaris
-          </h1>
-          <p>
-            Dirección: Michel Pax
-            <br>
-            Producido por: <a href="https://facebook.com/CoyoteFilmsMx">Coyote Films</a>
-<br><br>
-<small>
-  Producción: Kennya Salazar
-  <br>
-  Fotografía: Billy Bulnes
-  <br>
-  Diseño de Producción: Manuel González
-  <br>
-  Maquillaje: Victoria Hersan
-  <br>
-  Post Producción: Lou Key
-  <br>
-  Actriz: Ana Rosa Dávila
-  <br>
-  Actor: Diego Otero
-</small>
-          </p>
-        </div>
-      </div>
-      <div class="col-sm-7">
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/GmivsQUxOHw" frameborder="0" allowfullscreen></iframe>
-      </div>
-    </div>
-  </div>
-  <div class="container section">
-    <div class="row">
-      <div class="col-sm-8">
-        <h3>Biografía</h3>
-        <p>
-          Originarios de Chihuahua, México. Su potente y enérgico sonido hace remembranza del rock clásico. En 2013 estrenaron su primera producción homónima, pero su carrera comenzó mucho antes en Chihuahua 2007; en 2008 decidieron mudarse a Monterrey para seguir con la consolidación de su carrera.
-          <br>
-En el 2012 destacan con su participación en el Maquinaria Fest compartiendo escenario con Deftones, Marilyn Manson, Prodigy entre otros. La gira de su primer EP en cerró con más de 30 shows por toda la república mexicana no solo en venues locales sino también en el Vive Latino (2013), House of Vans y otros, cerrando con broche de oro al ser invitados por Dave Grohl (Foo Fighters) para abrir sus shows en México en el Foro Sol presentándose ante más de 25 mil personas.
-<br><br>
-En 2014 lanzaron su segunda producción Tercer solar que los llevó a nominaciones en los IMAS y les permitió hacer su primera gira fuera de México. La gira de Terce Solar recorrió 25 ciudades con 36 shows de los cuales 8 fueron sold out.
-<br><br>
-En 2015 lanzaron “Guardián” material producido por Omar Rodríguez López (Mars Volta/ At The Driving / Antemasque) mostrando un sonido que maduró con el tiempo pesentándolo en vivo en 42 fechas a lo largo del año y llevándolos a ganar el premio a Mejor Acto en Vivo de los IMAS. Para terminar el año se presentaron en los dos días
-del Corona Capital y en el Palacio de los Deportes junto a System of A Down. Este mismo año se posicionaron en el número 11 del conteo general de Reactor, además de ser seleccionados como una de las 10 bandas consideradas próximos headliners de México según Corona Descubre tu música.
-<br><br>
-En 2016 Apolo fue el gran ganador de los Indie O Awards que celebran a la música independiente, qsiendo la banda independiente de rock más importante del país.
-        </p>
-      </div>
-      <div class="col-sm-4">
-        <h3>Música</h3>
-        <div class="media album">
-          <div class="media-left">
-            <img src="<?php echo get_bloginfo('template_directory');?>/assets/images/album-2.png" alt="disco 1">
-          </div>
-          <div class="media-body">
-            <h5>Guardián</h5>
-            <em>2015</em>
-            <a href="https://open.spotify.com/album/2j4EBoJGKdSRkgPJY64DNH">Escuchar en spotify</a>
-            <a href="https://itunes.apple.com/mx/album/guardian/id1042513569">Comprar</a>
-          </div>
-        </div>
-        <div class="media album">
-          <div class="media-left">
-            <img src="<?php echo get_bloginfo('template_directory');?>/assets/images/album-3.png" alt="disco 1">
-          </div>
-          <div class="media-body">
-            <h5>Tercer Solar</h5>
-            <em>2014</em>
-            <a href="https://open.spotify.com/album/1lxfTi3MA49kZXI8TP6OHd">Escuchar en spotify</a>
-            <a href="https://www.kichink.com/buy/897815/apolomx/tercer-solar">Comprar</a>
-          </div>
-        </div>
-        <div class="media album">
-          <div class="media-left">
-            <img src="<?php echo get_bloginfo('template_directory');?>/assets/images/album-1.png" alt="disco 1">
-          </div>
-          <div class="media-body">
-            <h5>Apolo EP</h5>
-            <em>2012</em>
-            <a href="https://open.spotify.com/album/2UOvnnJkly6Yjgk83RouA2">Escuchar en spotify</a>
-            <a href="https://www.kichink.com/buy/897818/apolomx/apolo-ep">Comprar</a>
+  <div class="presskit">
+    <div class="presskit-content">
+      <div class="top-header text-center">
+        <div class="container">
+          <div class="social-icons block">
+            <ul class="list-inline">
+              <li><a href="https://www.facebook.com/apolomx/?fref=ts"><img src="<?php echo get_bloginfo('template_directory');?>/assets/images/icons/facebook.png" alt=""></a></li>
+              <li><a href="https://www.instagram.com/apolomx/"><img src="<?php echo get_bloginfo('template_directory');?>/assets/images/icons/instagram.png" alt=""></a></li>
+              <li><a href="https://www.youtube.com/user/APOLOMXTV"><img src="<?php echo get_bloginfo('template_directory');?>/assets/images/icons/youtube.png" alt=""></a></li>
+              <li><a href="https://twitter.com/apolomx"><img src="<?php echo get_bloginfo('template_directory');?>/assets/images/icons/twitter.png" alt=""></a></li>
+            </ul>
           </div>
         </div>
       </div>
-    </div>
-  </div>
-  <div class="tour section">
-    <div class="container">
-      <h1 class="text-center">Próximas fechas</h1>
-      <script type='text/javascript' src='http://widget.bandsintown.com/javascripts/bit_widget.js'></script><a href="http://www.bandsintown.com/Apolo" class="bit-widget-initializer" data-artist="Apolo" data-prefix="fbjs">Apolo Tour Dates</a>
-    </div>
-  </div>
-  <div class="footer section">
-    <div class="container">
-      <div class="row">
-        <div class="col-sm-4">
-          <h3>Management</h3>
-          Cesar Rosas
-          <br>
-          <a href="mailto:writetocesar@hotmail.com">writetocesar@hotmail.com</a>
-          <br>
-          <a href="tel:+5215544995266">(+52) 1 55 4499-5266</a>
-          <br><br>
-          Francisco Pérez Mejía
-          <br>
-          <a href="mailto:frasko@313agency.com">frasko@313agency.com</a>
-          <br>
-          <a href="+5215534025038">(+52) 1 55 3402-5038</a>
+      <div class="container">
+        <div class="text-center">
+          <img src="<?php echo get_bloginfo('template_directory');?>/assets/images/logo-pk.png" width="150">
+          <h1>Presskit Apolo</h1>
+        </div>
 
-          <h3>Booking</h3>
-          Connie Fuentes
-          <br>
-          <a href="mailto:conafuentes@gmail.com">conafuentes@gmail.com</a>
-          <br>
-          <a href="tel:+5215568172758">(+52)1 55 6817-2758</a>
+        <div class="bio section">
+          <h2 class="section-title">Biografía</h2>
+          <div class="row">
+            <div class="col-sm-6">
+              <img src="<?php echo get_bloginfo('template_directory');?>/assets/images/foto-pk.png" class="img-responsive">
+            </div>
+            <div class="col-sm-6">
+              <small>
+
+                Originarios de Chihuahua, México. Su potente y enérgico sonido hace remembranza del rock clásico. En 2013 estrenaron su primera producción homónima, pero su carrera comenzó mucho antes en Chihuahua 2007; en 2008 decidieron mudarse a Monterrey para seguir con la consolidación de su carrera.
+                <br><br>
+                En el 2012 destacan con su participación en el Maquinaria Fest compartiendo escenario con Deftones, Marilyn Manson, Prodigy entre otros. La gira de su primer EP en cerró con más de 30 shows por toda la república mexicana no solo en venues locales sino también en el Vive Latino (2013), House of Vans y otros, cerrando con broche de oro al ser invitados por Dave Grohl (Foo Fighters) para abrir sus shows en México en el Foro Sol presentándose ante más de 25 mil personas.
+    <br><br>
+                En 2014 lanzaron su segunda producción Tercer solar que los llevó a nominaciones en los IMAS y les permitió hacer su primera gira fuera de México. La gira de Terce Solar recorrió 25 ciudades con 36 shows de los cuales 8 fueron sold out.
+<br><br>
+                En 2015 lanzaron “Guardián” material producido por Omar Rodríguez López (Mars Volta/ At The Driving / Antemasque) mostrando un sonido que maduró con el tiempo pesentándolo en vivo en 42 fechas a lo largo del año y llevándolos a ganar el premio a Mejor Acto en Vivo de los IMAS. Para terminar el año se presentaron en los dos días
+                del Corona Capital y en el Palacio de los Deportes junto a System of A Down. Este mismo año se posicionaron en el número 11 del conteo general de Reactor, además de ser seleccionados como una de las 10 bandas consideradas próximos headliners de México según Corona Descubre tu música.
+<br><br>
+                En 2016 Apolo fue el gran ganador de los Indie O Awards que celebran a la música independiente, qsiendo la banda independiente de rock más importante del país.
+              </small>
+            </div>
+          </div>
         </div>
-        <div class="col-sm-8">
-          <h3>Contrataciones</h3>
-          <?php echo do_shortcode( '[contact-form-7 id="8" title="Contact form 1"]' ); ?>
+
+        <div class="row">
+          <div class="col-sm-6">
+            <div class="section">
+              <h2 class="section-title">
+                Escucha
+              </h2>
+              <iframe src="https://embed.spotify.com/?uri=spotify%3Aalbum%3A2j4EBoJGKdSRkgPJY64DNH" width="100%" height="400" frameborder="0" allowtransparency="true"></iframe>
+            </div>
+            <div class="section">
+              <h2 class="section-title">
+                Prensa
+              </h2>
+
+
+<h4><a target="_blank" href="http://rollingstone.com.mx/videos/apolo-estrena-el-video-de-jupiter-su-nuevo-sencillo/">Rolling Stone</a></h4>
+
+<h4><a target="_blank" href="http://www.indierocks.mx/musica/noticias/escucha-el-nuevo-sencillo-de-apolo/">Indie Rocks</a></h4>
+
+<h4><a target="_blank" href="http://marvin.com.mx/?s=apolo">Revista Marvin</a></h4>
+
+<h4><a target="_blank" href="http://remezcla.com/music/video-apolo-siddhartha-ft-omar-rodriguez-lopez">Remezcla (NY)</a></h4>
+
+<h4><a target="_blank" href="http://warp.la/warp-presenta-entrevista-con-apolo-77651">WARP</a></h4>
+
+<h4><a target="_blank" href="http://revesonline.com/2015/07/03/entrevista-con-apolo-la-banda-de-chihuahua-que-rockea-con-los-grandes/">Revés</a></h4>
+
+<h4><a target="_blank" href="http://radiotonica.com/la-noche-del-guardian-de-apolo">Radio Tónica</a></h4>
+
+<h4><a target="_blank" href="http://revistakuadro.com/la-potencia-y-energia-de-apolo-hara-que-no-los-olvides">Revista Kuadro</a></h4>
+
+<h4><a target="_blank" href="http://www.labandaelastica.com/indie-corner/apolo-energia-desgarradora#sthash.CMfroGIY.dpbs">La Banda Elástica</a></h4>
+
+<h4><a target="_blank" href="https://www.maspormas.com/2016/02/08/entrevista-con-la-banda-apolo">Más por Más</a></h4>
+
+<h4><a target="_blank" href="http://www.bizarro.fm/2015/10/mucho-rock-con-apolo-y-disidente-en-el-foro-indie-rocks/">Bizarro FM</a></h4>
+
+
+
+            </div>
+          </div>
+          <div class="col-sm-6">
+
+
+            <div class="section">
+              <h2 class="section-title">Festivales</h2>
+              <small>
+                Hellow Fest 2009
+                <br>
+                Maquinaria 2012 (Ft. Deftones y Marilyn Manson)
+                <br>
+                Vive Latino 2013
+                <br>
+                Foo Fighters (Opening Act) 2013
+                <br>
+                Festival Marvin 2014
+                <br>
+                House Of Vans 2014
+                <br>
+                Indie Fest 2014
+                <br>
+                Festival Alerta 2014
+                <br>
+                Rock En El Blanca 2014
+                <br>
+                Gira por Centroamérica 2014
+                <br>
+                RMX 212 Cancún 2014
+                <br>
+                Kadavar (Opening Act) 2014
+                <br>
+                Indie Fest 2015
+                <br>
+                SXSW 2015
+                <br>
+                Pal´ Norte 2015
+                <br>
+                Foro Indie Rocks (Presentación de "Guardián")
+                <br>
+                System Of A Down (Opening Act) 2015
+                <br>
+                Corona "Descubre Tu Música" 2015
+                <br>
+                Coheed & Cambria (Opening Act) 2015
+                <br>
+                Corona Capital (2 días) 2015
+                <br>
+                Festival de Rock Sinaloa 2015
+                <br>
+                Rockomiteco 2015
+                <br>
+                Vive Latino 2016
+                <br>
+                Festival Marvin 2016
+                <br>
+                Semana de las Juventudes 2016
+                <br>
+                Corona Revolution 2016
+                <br>
+                Gira de Universidades Marvin 2016
+                <br>
+              </small>
+            </div>
+
+            <div class="section">
+              <h2 class="section-title">
+                Puntos importantes
+              </h2>
+              <small>
+
+                Representantes de Shure en Expo Soundcheck 2013
+                <br>
+                Imagen de Vans en 2014
+                <br>
+                Embajadores de Buena Voluntad Nicaragua 2014
+                <br>
+                Ganadores de Mejor Acto en Vivo IMAS 2015
+                <br>
+                Imagen e Influencers de Sony Xperia, Dr. Martens y Levi´s.
+                <br>
+                Ganadores de Mejor Disco Rock, Mejor Acto en Vivo y Mejor Productor (Omar Rodríguez- López) IMAS 2016
+                <br>
+                Promedio de 42 fechas por año en la República Mexicana.
+
+              </small>
+            </div>
+
+          </div>
         </div>
+
+        <div class="section text-center">
+          <h2 class="section-title"><a href="https://www.dropbox.com/sh/t5hp1knzm0vsx4c/AAAY1kDoG14Gtrj_neBZu_esa?dl=0">Descarga fotos oficiales</a></h2>
+          <h2 class="section-title"><a href="https://www.dropbox.com/sh/v26boubrl1aji35/AADv1lG6qypjqDHtujha6Y-8a?dl=0">Descarga logotipo</a></h2>
+        </div>
+
+        <div class="section text-center">
+          <h2 class="section-title">
+            Videos de Youtube
+          </h2>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/Fsb6c-BKj9c" frameborder="0" allowfullscreen></iframe>
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/GmivsQUxOHw" frameborder="0" allowfullscreen></iframe>
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/T_teOgbDt_8" frameborder="0" allowfullscreen></iframe>
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/tEeRaRAkseA" frameborder="0" allowfullscreen></iframe>
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/SbVpQXnNL6w" frameborder="0" allowfullscreen></iframe>
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/H0sh65YCrkM" frameborder="0" allowfullscreen></iframe>
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/caT1FbCKnxA" frameborder="0" allowfullscreen></iframe>
+        </div>
+
+
+
+
       </div>
     </div>
   </div>
+
+
+  <div class="section">
+<div class="container">
+<h1 class="text-center">Próximas fechas</h1>
+<script type='text/javascript' src='http://widget.bandsintown.com/javascripts/bit_widget.js'></script><a class="bit-widget-initializer" href="http://www.bandsintown.com/Apolo" data-artist="Apolo" data-prefix="fbjs">Apolo Tour Dates</a>
+
+</div>
+</div>
+<div class="footer section">
+<div class="container">
+<div class="row">
+<div class="col-sm-4">
+<h3>Management</h3>
+Cesar Rosas
+
+<a href="mailto:writetocesar@hotmail.com">writetocesar@hotmail.com</a>
+
+<a href="tel:+5215544995266">(+52) 1 55 4499-5266</a>
+<h3>Booking</h3>
+Connie Fuentes
+
+<a href="mailto:conafuentes@gmail.com">conafuentes@gmail.com</a>
+
+<a href="tel:+5215568172758">(+52)1 55 6817-2758</a>
+
+</div>
+<div class="col-sm-8">
+<h3>Contrataciones</h3>
+<?php echo do_shortcode( '[contact-form-7 id="8" title="Contact form 1"]' ); ?>
+</div>
+</div>
+</div>
+</div>
+
 <?php endwhile; ?>
--- wp-content/themes/sage-master/template-custom.php
+++ wp-content/themes/sage-master/template-custom.php
@@ -5,6 +5,29 @@
 ?>
 
 <?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/page', 'header'); ?>
+
+  <div class="top-header text-center">
+    <div class="container">
+      <div class="social-icons block">
+        <ul class="list-inline">
+          <li><a href="https://www.facebook.com/apolomx/?fref=ts"><img src="<?php echo get_bloginfo('template_directory');?>/assets/images/icons/facebook.png" alt=""></a></li>
+          <li><a href="https://www.instagram.com/apolomx/"><img src="<?php echo get_bloginfo('template_directory');?>/assets/images/icons/instagram.png" alt=""></a></li>
+          <li><a href="https://www.youtube.com/user/APOLOMXTV"><img src="<?php echo get_bloginfo('template_directory');?>/assets/images/icons/youtube.png" alt=""></a></li>
+          <li><a href="https://twitter.com/apolomx"><img src="<?php echo get_bloginfo('template_directory');?>/assets/images/icons/twitter.png" alt=""></a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+  <div class="header text-center">
+    <div class="container">
+      <div class="photo-container">
+        <img src="<?php echo get_bloginfo('template_directory');?>/assets/images/photo.png" alt="" class="photo img-responsive center-block">
+      </div>
+      <img src="<?php echo get_bloginfo('template_directory');?>/assets/images/noise-logo.png" alt="" class="photo-noise img-responsive center-block">
+      <img src="<?php echo get_bloginfo('template_directory');?>/assets/images/logo.png" alt="" class="logo img-responsive">
+    </div>
+  </div>
+
   <?php get_template_part('templates/content', 'page'); ?>
 <?php endwhile; ?>
